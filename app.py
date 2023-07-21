2c2d9b375bb71fe1da6c109bd4aa3b64784ed2e4
meeheer123 <127537487+meeheer123@users.noreply.github.com>
2023-07-21 18:55:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ if __name__ == "__main__":
         # Create all tables defined in the models
         db.create_all()
     
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
